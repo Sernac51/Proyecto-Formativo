f75b6b0ce5a374693ee2f2ae24a873a61fd37643
Sernac51 <samuelserna2005@gmail.com>
2022-10-03 08:23:53 -0500

    Inicio

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\controllers\UsuariosController;
+use App\Http\controllers\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('usuarios', UsuariosController::class);
+Route::resource('products', ProductsController::class);
